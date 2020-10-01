ca21d36a209174361baed54304ea134ddb3d0c8c
MircoLaFerrara <71811698+MircoLaFerrara@users.noreply.github.com>
2020-10-01 09:35:22 +0200

    versione finale

---
+++ AnagraficApp/AnagraficApp/ElencoStudenti.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AnagraficApp
+{
+    class ElencoStudenti
+    {
+        private List<Studente> studenti;
+        public ElencoStudenti()
+        {
+            studenti = new List<Studente>();
+        }
+        public void aggiungiStudente(Studente studente)
+        {
+            studenti.Add(studente);
+        }
+        public List<Studente> getElencoCompleto()
+        {
+            return studenti;
+        }
+        public void scrivi(string percorso)
+        {
+            FileStream flusso = new FileStream(percorso, FileMode.CreateNew, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(flusso);
+            foreach (Studente s in studenti)
+            {
+                    writer.WriteLine(s.getNome() + "," + s.getCognome() + "," + s.getLuogoNascita() + "," + s.getDataNascita() + "," + s.getCodiceFiscale() + "," + s.getClasse());
+                    writer.Flush();
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ AnagraficApp/AnagraficApp/Studente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagraficApp
+{
+    class Studente
+    {
+        private string nome;
+        private string cognome;
+        private string luogoNascita;
+        private DateTime dataNascita;
+        private string codiceFiscale;
+        private string classe;
+
+        public Studente(string nome, string cognome, string luogoNascita, DateTime dataNascita, string codiceFiscale, string classe)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.luogoNascita = luogoNascita;
+            this.dataNascita = dataNascita;
+            this.codiceFiscale = codiceFiscale;
+            this.classe = classe;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public string getCognome()
+        {
+            return cognome;
+        }
+        public string getLuogoNascita()
+        {
+            return luogoNascita;
+        }
+        public DateTime getDataNascita()
+        {
+            return dataNascita;
+        }
+        public string getCodiceFiscale()
+        {
+            return codiceFiscale;
+        }
+        public string getClasse()
+        {
+            return classe;
+        }
+    }
+}
